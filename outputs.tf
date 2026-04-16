3db4c102de0e85e64d873cb64e66cb1b7450e4e1
Frank Muller <fmuller@schubergphilis.com>
2026-04-16 18:59:16 +0200

    feat: add module outputs for namespace, queues, topics, and subscriptions

---
+++ outputs.tf
@@ -0,0 +1,37 @@
+output "id" {
+  value       = azurerm_servicebus_namespace.this.id
+  description = "The resource ID of the Service Bus namespace."
+}
+
+output "name" {
+  value       = azurerm_servicebus_namespace.this.name
+  description = "The name of the Service Bus namespace."
+}
+
+output "endpoint" {
+  value       = azurerm_servicebus_namespace.this.endpoint
+  description = "The FQDN of the Service Bus namespace (e.g. myns.servicebus.windows.net)."
+}
+
+output "identity" {
+  value = var.system_assigned_identity_enabled ? {
+    principal_id = azurerm_servicebus_namespace.this.identity[0].principal_id
+    tenant_id    = azurerm_servicebus_namespace.this.identity[0].tenant_id
+  } : null
+  description = "The system-assigned managed identity principal and tenant IDs. Null when system-assigned identity is disabled."
+}
+
+output "queue_ids" {
+  value       = { for k, v in azurerm_servicebus_queue.this : k => v.id }
+  description = "Map of queue name to queue resource ID."
+}
+
+output "topic_ids" {
+  value       = { for k, v in azurerm_servicebus_topic.this : k => v.id }
+  description = "Map of topic name to topic resource ID."
+}
+
+output "subscription_ids" {
+  value       = { for k, v in azurerm_servicebus_subscription.this : k => v.id }
+  description = "Map of 'topic/subscription' key to subscription resource ID."
+}
